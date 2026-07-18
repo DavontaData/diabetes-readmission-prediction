1762f1e0a2c14721351583e08885bdcf5f2470c0
DavontaData <davontacarruth@gmail.com>
2026-07-18 17:29:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 import pandas as pd
 import joblib
+
+if st.button("Predict Readmission Risk"):
+
+    prediction = model.predict(input_data)[0]
+
+    probability = model.predict_proba(input_data)[0][1]
+  st.success(...)
+st.error(...)
+st.info(...)
